bf23bc13cc4fafa6c562a46adf7ce3561bbbce36
Chetan Pawar <chetan.pawar@mactosys.com>
2025-03-06 18:25:19 +0530

    array,function,objects and javascript

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// Javascript : javascript is a programming language that is used for converting static web pages to interactive and dynamic web pages
+
+// What are arrays, functions and objects?
+// Arrays : an array in is used to store a collection of values, such as a list of numbers or a list of number .
+// function : A function is a block of code that performs a specific task or return a value.
+// object : An object is a non primptive data type which can hold multiple values or a combination of values and functions.
+
